ac55c2c5e3f16e70ff42b3cc1776608c228b39d1
jitender94136 <jitender94136@gmail.com>
2018-06-07 16:28:27 +0530

    number of records inserted is prefixed in the response.

--- src/main/java/in/flexsol/controller/dump/DumpController.java
+++ src/main/java/in/flexsol/controller/dump/DumpController.java
@@ -24,7 +24,7 @@ public class DumpController {
 		DumpService dumpService;
 	
 	    @RequestMapping(value="/dump",method=org.springframework.web.bind.annotation.RequestMethod.POST)
-	    public ResponseEntity<Integer> saveDump(@RequestBody String requestBody ,HttpServletRequest request) {
+	    public ResponseEntity<String> saveDump(@RequestBody String requestBody ,HttpServletRequest request) {
 				try {
 	    			 Dump dump = new Dump();
 	    			 if(requestBody != null && requestBody.trim().toString().isEmpty() == false) {
@@ -34,10 +34,10 @@ public class DumpController {
 	    			 dump.setDump(requestBody);
 			         dump.setOrigin(request.getRemoteAddr());
 			         int returnStatus = dumpService.saveDump(dump);
-			         return new ResponseEntity<Integer>(returnStatus, HttpStatus.OK);
+			         return new ResponseEntity<String>(returnStatus+"|5|3", HttpStatus.OK);
 	    		} catch(Exception e) {
 	    			e.printStackTrace();
-	    			return new ResponseEntity<Integer>(-1, HttpStatus.INTERNAL_SERVER_ERROR);
+	    			return new ResponseEntity<String>("-1", HttpStatus.INTERNAL_SERVER_ERROR);
 	    		}
 	    }
 	    
